09377b2c4907b1d26a247f41ad3dd8bccad029bd
Jess Tucker <tuckerjess92@gmail.com>
2020-10-26 11:13:22 -0500

    WAR deployment config

--- VideogameCrud/src/main/java/com/skilldistillery/jpacrud/VideogameCrudApplication.java
+++ VideogameCrud/src/main/java/com/skilldistillery/jpacrud/VideogameCrudApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.jpacrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.jpacrud")
-public class VideogameCrudApplication {
+public class VideogameCrudApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VideogameCrudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideogameCrudApplication.class, args);
